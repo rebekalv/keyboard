ae854c0201ebdfe65496ecf283811fece3242fbc
rebekalv <rebekk0607@hotmail.com>
2024-09-09 16:43:39 +0200

    found leds and diodes

--- cv/app.js
+++ cv/app.js
@@ -5,3 +5,16 @@ mobileMenu.addEventListener('click', function() {
     mobileMenu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+function copyMail() {  
+    // Copy the text inside the text field
+    navigator.clipboard.writeText('rebekk0607@hotmail.com');
+
+    let tooltip = document.getElementById("mailTooltip");
+    tooltip.innerHTML = "Copied";
+}
+
+function mailOutFunc() {
+    let tooltip = document.getElementById("mailTooltip");
+    tooltip.innerHTML = "";
+}
